7019e15434048c1eda3f7827c8cd545b235b15c4
motunrayo-o <motunrayo.ogunyinka@gmail.com>
2023-09-01 10:52:43 +0100

    added placeholder for games without images

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,7 +1,9 @@
-const getCroppedImageUrl = (url: string) => {
-  if (!url) return null;
-  let tokens = url.split("media/");
+import imagePlaceholder from "../assets/no-image-placeholder-6f3882e0.webp";
 
+const getCroppedImageUrl = (url: string) => {
+  if (!url) return imagePlaceholder;
+
+  let tokens = url.split("media/");
   return `${tokens[0]}media/crop/600/400/${tokens[1]}`;
 };
 
