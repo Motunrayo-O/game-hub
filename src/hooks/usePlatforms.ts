7f59c6740e69138dd8783979e69cb4d99d21e3c7
motunrayo-o <motunrayo.ogunyinka@gmail.com>
2023-10-07 00:53:39 +0100

    refactor: relocate Platform interface

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,7 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
-import { Platform } from "./useGames";
 import apiClient, { DataResponse } from "../services/api-client";
 import platforms from "../data/platforms";
+
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
 
 const usePlatforms = () => {
   return useQuery({
