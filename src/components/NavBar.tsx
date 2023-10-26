d1e01b45a7f2238209f27f5416d0c19d4cbde2d6
motunrayo-o <motunrayo.ogunyinka@gmail.com>
2023-10-26 17:48:57 +0100

    navigate home on logo click

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,14 +1,15 @@
-import React from "react";
 import { HStack, Image } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 import logo from "../assets/logo.webp";
 import ThemeToggle from "./ThemeToggle";
 import GameSearch from "./GameSearch";
-import useGameQueryStore, { GameQueryStore } from "../store";
 
 const NavBar = () => {
   return (
     <HStack padding="10px">
-      <Image src={logo} boxSize="60px"></Image>
+      <Link to="/">
+        <Image src={logo} boxSize="60px"></Image>
+      </Link>
       <GameSearch />
       <ThemeToggle />
     </HStack>
